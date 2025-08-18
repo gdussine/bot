f7c3462fffb8e2b09b93d68d5306049bd4ec3024
gdussine <gdussine2@gmail.com>
2025-08-18 04:12:28 +0200

    PlatformService correction

--- src/main/java/bot/platform/PlatformService.java
+++ src/main/java/bot/platform/PlatformService.java
@@ -14,82 +14,77 @@ import okio.Path;
 
 public class PlatformService extends TemplateBotService {
 
-    private ObjectMapper mapper;
+	private ObjectMapper mapper;
 
-    private static final String AUTH_FILENAME = "auth.json";
-    
+	private static final String AUTH_FILENAME = "auth.json";
 
-    public PlatformService() {
-        this.mapper = new ObjectMapper();
-    }
+	public PlatformService() {
+		this.mapper = new ObjectMapper();
+	}
 
-    public File getMainDirectory() {
-        File directory = Path.get(System.getProperty("user.home")).resolve("." + this.getBot().getName()).toFile();
-        directory.mkdir();
-        return directory;
-    }
+	public File getMainDirectory() {
+		File directory = Path.get(System.getProperty("user.home")).resolve("." + this.getBot().getName()).toFile();
+		directory.mkdir();
+		return directory;
+	}
 
-    public File getConfigDirectory() {
-        File directory = Path.get(getMainDirectory()).resolve("config").toFile();
-        directory.mkdir();
-        return directory;
-    }
+	public File getConfigDirectory() {
+		File directory = Path.get(getMainDirectory()).resolve("config").toFile();
+		directory.mkdir();
+		return directory;
+	}
 
-    public File getOutDirectory() {
-        File directory = Path.get(getMainDirectory()).resolve("out").toFile();
-        directory.mkdir();
-        return directory;
-    }
+	public File getOutDirectory() {
+		File directory = Path.get(getMainDirectory()).resolve("out").toFile();
+		directory.mkdir();
+		return directory;
+	}
 
-    public File getConfigFile(String fileName) {
-        File file = Path.get(getConfigDirectory()).resolve(fileName).toFile();
-        return file;
-    }
+	public File getConfigFile(String fileName) {
+		File file = Path.get(getConfigDirectory()).resolve(fileName).toFile();
+		return file;
+	}
 
-    public File createConfigFile(String configFileName, Object configObject) {
-        try {
-            File configFile = getConfigFile(configFileName);
-            configFile.createNewFile();
-            mapper.writeValue(configFile, configObject);
-            return configFile;
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-            return null;
-        }
-    }
+	public <T extends PlatformConfiguration> File createConfigFile(String configFileName, Class<T> configType) {
+		try {
+			File configFile = getConfigFile(configFileName);
+			configFile.createNewFile();
+			mapper.writeValue(configFile,  getDefaultConfiguration(configType));
+			return configFile;
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			return null;
+		}
+	}
 
-    public File loadConfigFile(String fileName, Object defaultObject) {
-        File file = getConfigFile(fileName);
-        if (file.exists()) {
-            return file;
-        }
-        return createConfigFile(fileName, defaultObject);
-    }
+	public <T extends PlatformConfiguration> T getDefaultConfiguration(Class<T> configClass) {
+		try {
+			T config = configClass.getConstructor().newInstance();
+			config.setDefaultConfiguration();
+			return config;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
-    public <T extends PlatformConfiguration> T getDefaultConfiguration(Class<T> configClass) {
-        try {
-            T config = configClass.getConstructor().newInstance();
-            config.setDefaultConfiguration();
-            return config;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+	public <T extends PlatformConfiguration> T getPlatformConfiguration(String fileName, Class<T> configType) {
+		File configFile = getConfigFile(fileName);
+		if (!configFile.exists()) {
+			configFile = createConfigFile(fileName, configType);
+		}
+		try (InputStream in = new FileInputStream(configFile)) {
+			return new ObjectMapper().readValue(in, configType);
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+			return null;
+		}
 
-    public <T extends PlatformConfiguration> T getPlatformConfiguration(String filename, Class<T> configClass) {
-        File configFile = loadConfigFile(filename, configClass);
-        try (InputStream in = new FileInputStream(configFile)) {
-            return new ObjectMapper().readValue(in, configClass);
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-            return null;
-        }
+	}
 
-    }
+	public BotConfiguration getBotConfiguration() {
 
-    public BotConfiguration getBotConfiguration() {
-        return getPlatformConfiguration(AUTH_FILENAME, BotConfiguration.class);
-    }
+		return getPlatformConfiguration(AUTH_FILENAME, BotConfiguration.class);
+	}
 
 }
